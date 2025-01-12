2de826cbdbbd6d78f9bb2e621df3fbc502a1cfae
Chris Tarroja <ctglabsdev@gmail.com>
2025-01-12 17:06:37 +0800

    first commit

---
+++ src/app/[...url]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface PageProps {
+  params: {
+    url: string | string[] | undefined;
+  }
+};
+
+const Page = ({ params }: PageProps) => {
+  console.log("params", params);
+  return (
+    <div>Hello!</div>
+  );
+}
+
+export default Page
