22b873deaf55b0aff5d7040ed33e5d0135512350
Chris Tarroja <ctglabsdev@gmail.com>
2025-01-12 17:57:10 +0800

    add indexing of websites using redis

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import { ragChat } from '../lib/rag-chat';
+import { redis } from '../lib/redis';
 
 interface PageProps {
-  params: {
-    url: string | string[] | undefined;
-  }
+  params: Promise<{ url: string | string[] | undefined }>;
 };
 
-const Page = ({ params }: PageProps) => {
-  console.log("params", params);
+function reconstructUrl({ url }: { url: string[] }) {
+  const decodedComponents = url.map((component) => decodeURIComponent(component));
+
+  return decodedComponents.join("/");
+};
+
+const Page = async ({ params }: PageProps) => {
+  const { url } = await params;
+  const recostructedUrl = reconstructUrl({ url: url as string[] });
+
+  const isAlreadyIndexed = await redis.sismember("indexedUrls", recostructedUrl);
+
+  if (!isAlreadyIndexed) {
+    await ragChat.context.add({
+      type: "html",
+      source: recostructedUrl,
+      config: { 
+        chunkOverlap: 50,
+        chunkSize: 200,
+      },
+    });
+
+    await redis.sadd("indexedUrls", recostructedUrl);
+  }
+
+  console.log("isAlreadyIndexed? ", isAlreadyIndexed);
+
   return (
     <div>Hello!</div>
   );
